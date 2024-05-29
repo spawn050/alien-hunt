e47e041b4c76d74434719c6365a1b39686e49ce6
Omkar Satam <omkarsatam050@gmail.com>
2024-05-29 15:58:29 +0530

    create a basic game loop with fps limit

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
+    Color grey = {29, 29, 27, 255};
     int windowWidth = 750;
     int windowHeight = 700;
 
     InitWindow(windowWidth, windowHeight, "Alien Hunt");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(grey);
+        EndDrawing();
+    }
+    
+
     CloseWindow();
 }
